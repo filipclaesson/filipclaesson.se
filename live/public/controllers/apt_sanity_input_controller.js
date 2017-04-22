7da7166539ba0b298435e1b2324ab4b911254dd3
filip Claesson <filip.claesson@klarna.com>
2017-04-22 23:48:15 +0200

    Bootstrap input

--- live/public/controllers/apt_sanity_input_controller.js
+++ live/public/controllers/apt_sanity_input_controller.js
@@ -1,3 +1,17 @@
 myApp.controller('AptSanityInputController', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from apts sanity input");
-      }]);
+   
+   $scope.test = 'janne'
+   $scope.apt = {};
+   $scope.ResPricePerM2 = "";
+
+    $scope.update = function(){
+   		$scope.computePricePerM2();
+   		$scope.visualizeProximityApt();
+    }
+    $scope.computePricePerM2 = function(){
+    	res = $scope.apt.price/$scope.apt.m2;
+    	$scope.ResPricePerM2 = Math.round(res*1000)/1000;
+    }
+
+ }]);
