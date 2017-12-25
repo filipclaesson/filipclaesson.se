1eef4cf253ba46db5de5545156e2e16749d33a0e
Filip Claesson <Filip@Filips-MacBook-Air.local>
2017-12-25 23:26:17 +0100

    only sthlm

--- live/public/controllers/analysis_controller.js
+++ live/public/controllers/analysis_controller.js
@@ -2,9 +2,10 @@
 myApp.controller('AnalysisController', ['$scope', '$http', function($scope, $http) {
 //console.log("Hello World from analysis controller");
 
-plotChartDate('quarter','round' ,'%', 'Prisutveckling per stadsdel (%)', "price_development_percent", "select areas, quarter, round*100 as round from analysis_test where areas in ('Hammarby Sjöstad','Hammarbyhöjden','Södermalm','Årsta','Liljeholmen','Aspudden','Östermalm','Kungsholmen','Gröndal','Vasastan','Älvsjö','Enskede','Midsommarkransen','Telefonplan','Hägersten','Bromma')")
-plotChartDate('quarter','round' ,'Tkr', 'Prisutveckling per stadsdel (Tkr/sqm)', "price_development_absolute", "select area_from_geo as areas , month as quarter , round(avg(sqm_sold_price)) as round from booli.analysis_base where sqm between 40 and 80 and area_from_geo <> 'Odefinierad' group by 1,2 ")
-plotChartText('sqm_bucket','bid_increase' ,'%', 'Prisuppgång från utgångsbud per stadsdel och storlek (%)', "price_up_development_percent", "select  areas , sqm_bucket ,round(avg(bid_increase::numeric*100),1) as bid_increase from analysis_base where bid_increase <> 'NaN' and room is not null and areas_count > 300 and areas in ('Hammarby Sjöstad','Hammarbyhöjden','Södermalm','Årsta','Liljeholmen','Aspudden','Östermalm','Kungsholmen','Gröndal','Vasastan','Älvsjö','Enskede','Midsommarkransen','Telefonplan','Hägersten','Bromma') group by 1,2 order by 1,2 ")
+//plotChartDate('quarter','round' ,'%', 'Prisutveckling per stadsdel (%)', "price_development_percent", "select areas, quarter, round*100 as round from analysis_test where areas in ('Hammarby Sjöstad','Hammarbyhöjden','Södermalm','Årsta','Liljeholmen','Aspudden','Östermalm','Kungsholmen','Gröndal','Vasastan','Älvsjö','Enskede','Midsommarkransen','Telefonplan','Hägersten','Bromma')")
+//plotChartDate('month','round' ,'Tkr', 'Prisutveckling per stadsdel (Tkr/sqm)', "price_development_absolute", "select area_from_geo as areas , month as month , round(avg(sqm_sold_price)) as round from booli.analysis_base where sqm between 40 and 80 and area_from_geo in ('Södermalm','Kungsholmen','Vasastan','Östermalm','Råsunda','Årsta','Gärdet','Sundbyberg','Huvudsta','Hammarbyhamnen','Midsommarkransen','Hammarbyhöjden','Liljeholmen','Hagalund','Fruängen','Mariehäll','Aspudden','Järva','Globen','Gamla Enskede','Traneberg','Norrmalm') group by 1,2 ")
+plotChartDate('month','round' ,'Tkr', 'Prisutveckling per stadsdel (Tkr/sqm)', "price_development_absolute", "select month as month , round(avg(sqm_sold_price)) as round from booli.analysis_base where sqm between 40 and 80 and area_from_geo in ('Södermalm') group by 1 order by 1")
+//plotChartText('sqm_bucket','bid_increase' ,'%', 'Prisuppgång från utgångsbud per stadsdel och storlek (%)', "price_up_development_percent", "select  areas , sqm_bucket ,round(avg(bid_increase::numeric*100),1) as bid_increase from analysis_base where bid_increase <> 'NaN' and room is not null and areas_count > 300 and areas in ('Hammarby Sjöstad','Hammarbyhöjden','Södermalm','Årsta','Liljeholmen','Aspudden','Östermalm','Kungsholmen','Gröndal','Vasastan','Älvsjö','Enskede','Midsommarkransen','Telefonplan','Hägersten','Bromma') group by 1,2 order by 1,2 ")
 
 function plotChartDate(x_data, y_data, yaxis_title, title, graph_name,query_in){
     // query_in = "select * from analysis_test where areas in ('Bromma','Bagarmossen')"
@@ -41,46 +42,51 @@ function plotChartText(x_data, y_data, yaxis_title, title, graph_name,query_in){
 }
 
 
+
 function structureDataDate(data, x_data, y_data, graph_name, title, yaxis_title){
     x = [];
     y = [];
     plot_data = []
-    y.push(data[0][y_data])
-    date = new Date(data[0][x_data]);
-    //console.log(String(date.getFullYear()) + '-Q' + String(Math.floor((date.getMonth() + 3) / 3)))
-    date = String(date.getFullYear()) + '-Q' + String(Math.floor((date.getMonth() + 3) / 3))
-    x.push(date)
+    
+
+    
     for (var i = 1; i < data.length; i++) {
-        if (data[i]["areas"] == data[i-1]["areas"]){
-            //console.log("old area: " + data[i]["areas"])
-            y.push(data[i][y_data])
-            date = new Date(data[i][x_data])
-            //console.log(String(date.getFullYear()) + '-Q' + String(Math.floor((date.getMonth() + 3) / 3)))
-            date = String(date.getFullYear()) + '-Q' + String(Math.floor((date.getMonth() + 3) / 3))
-            x.push(date)
-        }else{
-            //console.log("new area: " + data[i]["areas"])
-            plot_data.push({
-                x:x,
-                y:y,
-                mode: 'lines',
-                name: data[i-1]["areas"]
-            });
-            x = [];
-            y = [];
-            y.push(data[i][y_data])
-            date = new Date(data[i][x_data])
-            //console.log(String(date.getFullYear()) + '-Q' + String(Math.floor((date.getMonth() + 3) / 3)))
-            date = String(date.getFullYear()) + '-Q' + String(Math.floor((date.getMonth() + 3) / 3))
-            x.push(date);
-        }
+        date = new Date(data[i][x_data]).toISOString().slice(0,10);
+        console.log(date)
+
+        x.push(date)
+        y.push(data[i][y_data])
+
+        // if (data[i]["areas"] == data[i-1]["areas"]){
+        //     //console.log("old area: " + data[i]["areas"])
+        //     y.push(data[i][y_data])
+        //     date = new Date(data[i][x_data])
+        //     //console.log(String(date.getFullYear()) + '-Q' + String(Math.floor((date.getMonth() + 3) / 3)))
+        //     //date = String(date.getFullYear()) + '-Q' + String(Math.floor((date.getMonth() + 3) / 3))
+        //     x.push(date)
+        // }else{
+        //     //console.log("new area: " + data[i]["areas"])
+        //     plot_data.push({
+        //         x:x,
+        //         y:y,
+        //         mode: 'lines',
+        //         name: data[i-1]["areas"]
+        //     });
+        //     x = [];
+        //     y = [];
+        //     y.push(data[i][y_data])
+        //     date = new Date(data[i][x_data])
+        //     //console.log(String(date.getFullYear()) + '-Q' + String(Math.floor((date.getMonth() + 3) / 3)))
+        //     //date = String(date.getFullYear()) + '-Q' + String(Math.floor((date.getMonth() + 3) / 3))
+        //     x.push(date);
+        // }
     };
     
     plot_data.push({
         x:x,
         y:y,
         mode: 'lines',
-        name: data[data.length-1]["areas"]
+        name: 'Stockholm'
     });
     console.log(plot_data);
 
@@ -99,7 +105,7 @@ function structureDataDate(data, x_data, y_data, graph_name, title, yaxis_title)
     };
 
     PLOT = document.getElementById(graph_name);
-    Plotly.plot( PLOT, plot_data, layout);
+    Plotly.plot(PLOT, plot_data, layout);
 
 }
 
@@ -109,8 +115,10 @@ function structureDataText(data, x_data, y_data, graph_name, title, yaxis_title)
     x = [];
     y = [];
     plot_data = []
+
     y.push(data[0][y_data])
     x.push(data[0][x_data])
+
     for (var i = 1; i < data.length; i++) {
         if (data[i]["areas"] == data[i-1]["areas"]){
             //console.log("old area: " + data[i]["areas"])
@@ -139,7 +147,7 @@ function structureDataText(data, x_data, y_data, graph_name, title, yaxis_title)
         mode: 'bar',
         name: data[data.length-1]["areas"]
     });
-    console.log(plot_data);
+    // console.log(plot_data);
 
 
     var layout = {
